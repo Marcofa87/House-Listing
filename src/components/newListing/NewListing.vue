7040884082b4523c5de5587cac5fdbd963dc886a
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-07-12 12:41:23 +0200

    test

--- src/components/newListing/NewListing.vue
+++ src/components/newListing/NewListing.vue
@@ -1,4 +1,71 @@
-<template><h2>New Listing</h2></template>
-<script lang="ts" setup></script>
+<template>
+  <div class="new-listing">
+    <div class="header">
+      <img src="../../assets//ic_back_grey@3x.png" />
+      <h2>Create New Listing</h2>
+    </div>
+  </div>
+  <p>Message is:</p>
+  <input v-model="message" placeholder="edit me" />
+  <p>Message is:</p>
+  <input v-model="message" placeholder="edit me" />
+  <p>Message is:</p>
+  <input v-model="message" placeholder="edit me" />
+  <p>Message is:</p>
+  <input v-model="message" placeholder="edit me" />
+  <p>Message is:</p>
+  <input v-model="message" placeholder="edit me" />
+  <p>Message is:</p>
+  <input v-model="message" placeholder="edit me" />
+  <p>Message is:</p>
+  <input v-model="message" placeholder="edit me" />
+  <p>Message is:</p>
+  <input v-model="message" placeholder="edit me" />
+  <div>Selected: {{ selected }}</div>
+  <select v-model="selected">
+    <option disabled value="">Please select one</option>
+    <option>Yes</option>
+    <option>No</option>
+  </select>
+  <p>Message is:</p>
+  <input v-model="message" placeholder="edit me" />
+  <p>Message is:</p>
+  <input v-model="message" placeholder="edit me" />
+  <p>Message is:</p>
+  <input v-model="message" placeholder="edit me" />
+  <span>Multiline message is:</span>
+  <p style="white-space: pre-line">{{ message }}</p>
+  <textarea v-model="message" placeholder="add multiple lines"></textarea>
+  <CustomButtons>Price</CustomButtons>
+</template>
 
-<style scoped></style>
+<script setup>
+import CustomButtons from '@/shared/CustomButtons.vue'
+</script>
+
+<style scoped>
+.new-listing {
+  display: flex;
+  align-content: center;
+  padding: 20px;
+  border: 1px solid red;
+  width: 80%;
+  margin: 0 auto;
+}
+.header {
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  padding: 20px;
+}
+
+.header img {
+  width: 32px;
+}
+
+.header h2 {
+  text-align: center;
+  width: 50%;
+  margin: 0 auto;
+}
+</style>
--- src/views/NewListing.vue
+++ src/views/NewListing.vue
@@ -1,71 +1,7 @@
-<template>
-  <div class="new-listing">
-    <div class="header">
-      <img src="../assets//ic_back_grey@3x.png" />
-      <h2>Create New Listing</h2>
-    </div>
-  </div>
-  <p>Message is:</p>
-  <input v-model="message" placeholder="edit me" />
-  <p>Message is:</p>
-  <input v-model="message" placeholder="edit me" />
-  <p>Message is:</p>
-  <input v-model="message" placeholder="edit me" />
-  <p>Message is:</p>
-  <input v-model="message" placeholder="edit me" />
-  <p>Message is:</p>
-  <input v-model="message" placeholder="edit me" />
-  <p>Message is:</p>
-  <input v-model="message" placeholder="edit me" />
-  <p>Message is:</p>
-  <input v-model="message" placeholder="edit me" />
-  <p>Message is:</p>
-  <input v-model="message" placeholder="edit me" />
-  <div>Selected: {{ selected }}</div>
-  <select v-model="selected">
-    <option disabled value="">Please select one</option>
-    <option>Yes</option>
-    <option>No</option>
-  </select>
-  <p>Message is:</p>
-  <input v-model="message" placeholder="edit me" />
-  <p>Message is:</p>
-  <input v-model="message" placeholder="edit me" />
-  <p>Message is:</p>
-  <input v-model="message" placeholder="edit me" />
-  <span>Multiline message is:</span>
-  <p style="white-space: pre-line">{{ message }}</p>
-  <textarea v-model="message" placeholder="add multiple lines"></textarea>
-  <CustomButtons>Price</CustomButtons>
-</template>
-
-<script setup>
-import CustomButtons from '@/shared/CustomButtons.vue'
+<script setup lang="ts">
+import NewListing from '../components/newListing/NewListing.vue'
 </script>
 
-<style scoped>
-.new-listing {
-  display: flex;
-  align-content: center;
-  padding: 20px;
-  border: 1px solid red;
-  width: 80%;
-  margin: 0 auto;
-}
-.header {
-  width: 80%;
-  margin: 0 auto;
-  display: flex;
-  padding: 20px;
-}
-
-.header img {
-  width: 32px;
-}
-
-.header h2 {
-  text-align: center;
-  width: 50%;
-  margin: 0 auto;
-}
-</style>
+<template>
+  <NewListing />
+</template>
