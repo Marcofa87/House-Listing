0d9e66f440769539d4fcab1ef8114210f6f2bb52
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-07-19 20:00:12 +0200

    implemented uloading image store

--- src/stores/uploadImageStore.ts
+++ src/stores/uploadImageStore.ts
@@ -1,39 +1,52 @@
-const myHeaders = new Headers()
-myHeaders.append('X-Api-Key', 'FPNh7v3pOKHkqtEJ2IB1o8zjLWyAmrxg')
+// imageUploadStore.ts
+import { defineStore } from 'pinia'
 
-// Definizione di FormData
-const formdata = new FormData()
-const fileInput = document.querySelector('input[type="file"]') as HTMLInputElement
-if (fileInput && fileInput.files) {
-  formdata.append('image', fileInput.files[0], 'pexels-photo-1311518.jpeg')
+interface ImageUploadState {
+  isUploading: boolean
+  lastUploadedImageUrl: string | null
 }
 
-// Opzioni della richiesta
-const requestOptions: RequestInit = {
-  method: 'POST',
-  headers: myHeaders,
-  body: formdata,
-  redirect: 'follow'
-}
+export const useImageUploadStore = defineStore('imageUpload', {
+  state: (): ImageUploadState => ({
+    isUploading: false,
+    lastUploadedImageUrl: null
+  }),
+  actions: {
+    async uploadHouseImage(houseId: string, file: File) {
+      this.isUploading = true
 
-// Funzione per inviare la richiesta
-const uploadImage = async () => {
-  try {
-    console.log('Invio della richiesta...')
+      const myHeaders = new Headers()
+      myHeaders.append('X-Api-Key', 'FPNh7v3pOKHkqtEJ2IB1o8zjLWyAmrxg')
 
-    const response = await fetch(
-      'https://api.intern.d-tt.nl/api/houses/:houseId/upload',
-      requestOptions
-    )
+      const formData = new FormData()
+      formData.append('image', file, file.name)
 
-    console.log('Risposta ricevuta:', response)
+      const requestOptions: RequestInit = {
+        method: 'POST',
+        headers: myHeaders,
+        body: formData,
+        redirect: 'follow'
+      }
 
-    const result = await response.text()
+      try {
+        const response = await fetch(
+          `https://api.intern.d-tt.nl/api/houses/${houseId}/upload`,
+          requestOptions
+        )
+        const result = await response.json()
+        console.log(result)
 
-    console.log('Risultato:', result)
-  } catch (error) {
-    console.error('Errore:', error)
+        if (result.imageUrl) {
+          this.lastUploadedImageUrl = result.imageUrl
+        }
+
+        return result
+      } catch (error) {
+        console.error('error', error)
+        throw error
+      } finally {
+        this.isUploading = false
+      }
+    }
   }
-}
-
-// E
+})
